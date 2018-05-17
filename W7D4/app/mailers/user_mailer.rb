02df27e9006baae08bdaaf657a1cd1e6feb22ec1
Aude Cervantes <aude.cervantes@hotmail.fr>
2018-05-17 12:25:23 +0200

    add mailer

---
+++ W7D4/app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ApplicationMailer
+    default from: 'lilleinpanam@gmail.com'
+ 
+  def welcome_email
+    @user = params[:user]
+    @url  = 'https://w7d4.herokuapp.com/' # app heroku
+    mail(to: @user.email, subject: 'Rejoins la communauté des Hackeurs !')
+  end
+end
