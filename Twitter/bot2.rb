37c21138c55c36c2ee220e9dd497540f63c04014
bafraikin <fraikin.baptiste@gmail.com>
2018-05-17 11:14:49 +0200

    aller

--- Twitter/bot2.rb
+++ Twitter/bot2.rb
@@ -12,7 +12,6 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = ENV['access_token_secret']
 end
 
-
 client.search("start-up -rt", lang: "fr").take(1).collect do |object|
 	client.update("@#{object.user.screen_name} Je te présente The Hacking Project, une formation au code en pair-programming qui pourra t'aider dans ton projet, retrouve nous sur https://thplille.herokuapp.com/hackers ", in_reply_to_tweet_id: object.id )
 sleep(120)
