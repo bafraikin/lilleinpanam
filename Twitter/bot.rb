7136bb603faf56b515e4e6bcbc9276903bd8abc4
bafraikin <fraikin.baptiste@gmail.com>
2018-05-17 12:22:16 +0200

    aller

--- Twitter/bot.rb
+++ Twitter/bot.rb
@@ -12,7 +12,7 @@ end
 
 
 while true
-client.search("parcoursup -rt", lang: "fr").take(100).collect do | tweet | 
+client.search("parcoursup -rt", lang: "fr").take(5).collect do | tweet | 
 client.update("@#{tweet.user.screen_name} Peut etre que t'aurais envie de t'initier au code gratuitement, pendant les vacances, c'est ici -> https://thplille.herokuapp.com/hackers", in_reply_to_status_id: tweet.id, trim_user: true)
 puts "tweet envoyé à #{tweet.user.screen_name}"
 sleep 1800
