f7b20aeca7f49ece7f3b0f987e5a31ed05dee8e7
bafraikin <fraikin.baptiste@gmail.com>
2018-05-17 19:41:45 +0200

    heroku

--- W7D4/app/controllers/users/registrations_controller.rb
+++ W7D4/app/controllers/users/registrations_controller.rb
@@ -15,18 +15,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
     puts "--------2------"
     @user = User.new(params[:user])
     puts "New User"
-
     respond_to do |format|
       if @user.save
         # Tell the UserMailer to send a welcome email after save
-      #  UserMailer.with(user: @user).welcome_email.deliver_now
         puts "Mail envoyé"
 
-  
         format.html { redirect_to(@user, notice: 'User was successfully created.') }
         format.json { render json: @user, status: :created, location: @user }
-
         puts "sauvegarde de user"
+        UserMailer.with(user: @user).welcome_email.deliver_now
       else
         format.html { render action: 'new' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
